9db00735afec61af4a4dc370f29cb6f5fff45535
mezyapps <mezyapps@gmail.com>
2019-09-06 17:47:24 +0530

    NJ 1 Commit 06-10-2019

---
+++ app/src/main/java/com/mezyapps/new_reportanalyst/view/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mezyapps.new_reportanalyst.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mezyapps.new_reportanalyst.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/mezyapps/new_reportanalyst/view/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.mezyapps.new_reportanalyst.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mezyapps.new_reportanalyst.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
