80c546451cd2bf7e88de951a86e6dcfd1dd1b6cf
mezyapps <mezyapps@gmail.com>
2019-09-07 13:01:18 +0530

    Nj Commit 07-09-2019 1

---
+++ app/src/main/java/com/mezyapps/new_reportanalyst/view/activity/LicenseKeyActivity.java
@@ -0,0 +1,16 @@
+package com.mezyapps.new_reportanalyst.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mezyapps.new_reportanalyst.R;
+
+public class LicenseKeyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license_key);
+    }
+}
--- app/src/main/java/com/mezyapps/new_reportanalyst/view/activity/MainActivity.java
+++ app/src/main/java/com/mezyapps/new_reportanalyst/view/activity/MainActivity.java
@@ -1,16 +1,40 @@
 package com.mezyapps.new_reportanalyst.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.mezyapps.new_reportanalyst.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView iv_drawer;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        find_View_IdS();
+        events();
+    }
+
+    private void find_View_IdS() {
+        iv_drawer=findViewById(R.id.iv_drawer);
+        drawerLayout=findViewById(R.id.drawerLayout);
+    }
+
+    private void events() {
+        iv_drawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }
